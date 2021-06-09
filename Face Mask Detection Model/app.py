5c7b7f852a1b205f161d48240cd8cca04fe03075
Mirza-Abdul-Aziz <71326745+Mirza-Abdul-Aziz@users.noreply.github.com>
2021-06-09 10:38:53 +0500

    Add files via upload

---
+++ Face Mask Detection Model/app.py
@@ -0,0 +1,12 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+
